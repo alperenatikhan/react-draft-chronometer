d73b6b60b4a16e49c68383d282f061abb2190ed6
alperenatikhan <40471361+alperenatikhan@users.noreply.github.com>
2021-04-24 00:38:46 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,13 +4,54 @@ import "./style.css";
 export default function App() {
   const timerIdRef = useRef(0);
   const [count, setCount] = useState(0);
+  const [session, setSession] = useState(25);
+  const [breaking, setBreaking] = useState(5);
 
   const startHandler = () => {
     if (timerIdRef.current) {
       return;
     }
     timerIdRef.current = setInterval(() => setCount(c => c + 1), 1000);
+    
+    return sessionTimer;
   };
+
+const sessionTimer = (count, session) => {
+if (count < session*60){
+return `${Math.floor(((session * 60) - count) / 60)} : ${(60 - count) % 60}` ;
+}else{
+return "00:00"
+}
+}
+
+
+const breakingTimer =(count,breaking) => {
+
+if (count < breaking*60){
+return `${Math.floor(((breaking * 60) - count) / 60)} : ${(60 - count) % 60}`
+}else{
+return "00:00"
+}
+}
+
+
+  const breakingHandler = (event) =>{
+
+    if (event.target.value == "+"){
+  setBreaking((prevState) => prevState+1);
+}else if (event.target.value == "-"){
+  setBreaking((prevState) => prevState-1);
+    }
+
+  }
+
+  const sessionHandler = (event) => {
+    if (event.target.value == "+"){
+  setSession((prevState) => prevState+1);
+    }else if (event.target.value == "-"){
+  setSession((prevState) => prevState-1);
+    }
+  }
 
   const stopHandler = () => {
     clearInterval(timerIdRef.current);
@@ -30,7 +71,10 @@ export default function App() {
   return (
     <div>
       <h1>Chronometer Draft</h1>
-      <p> {`${Math.floor((600 - count) / 60)} : ${(600 - count) % 60}`}</p>
+      <div id= "break-display"><h3> Break </h3> <button value="+" onClick= {breakingHandler}>+</button> <button value= "-" onClick = {breakingHandler}>-</button><h4> {breaking} </h4></div>
+      <div id ="session-display"><h3> Session </h3> <button  value= "+" onClick= {sessionHandler}>+</button> <button value="-" onClick={sessionHandler}>-</button> <h4> {session} </h4></div>
+      <div id="session-chronometer"><p> {sessionTimer(count,session)? sessionTimer(count,session): "00:00" }</p> <p> Session Timer </p> </div>
+    <div id="break-chronometer"> <p> {breakingTimer(count,breaking)}</p> <p> Break Timer </p> </div>
       <button onClick={startHandler}>Start</button>
       <button onClick={stopHandler}>Pause</button>
       <button onClick={refreshHandler}>Stop</button>
